b6bcd3a8626d8446d14dc4afa8e05d824ac50023
justinwong2 <e1121317@u.nus.edu>
2026-06-30 10:15:36 +0800

    config

--- artifacts/digital-passport/api/users.ts
+++ artifacts/digital-passport/api/users.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { findUserByStaffId, createUser } from "./_lib/sheets.js";
+import { findUserByStaffId, createUser } from "./_lib/sheets";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== "POST") {
--- artifacts/digital-passport/api/users/[staffId]/booths/[boothId].ts
+++ artifacts/digital-passport/api/users/[staffId]/booths/[boothId].ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { updateBoothInSheet } from "../../../_lib/sheets.js";
+import { updateBoothInSheet } from "../../../_lib/sheets";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== "PATCH") {
