10f8ae1684a5b1b2a999a082106a26c065c60c3c
justinwong2 <e1121317@u.nus.edu>
2026-06-30 10:24:25 +0800

    module type

--- artifacts/digital-passport/api/users.ts
+++ artifacts/digital-passport/api/users.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { findUserByStaffId, createUser } from "./_lib/sheets";
+import { findUserByStaffId, createUser } from "./_lib/sheets.js";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== "POST") {
